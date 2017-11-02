7631b954f872460183c2daefbb3076eacacba4b1
vivek singh <er.vivek.it@gmail.com>
2017-11-02 15:46:58 +0530

    first commit

---
+++ data_insert.php
@@ -0,0 +1,17 @@
+
+
+<form action="">
+    code
+  <input type="text" name="product_code"><br>
+    pro code
+  <input type="text" name="product_name">
+    description
+  <input type="text" name="product_desc">
+     image
+
+  <input type="file" name="product_img_name">
+    price
+  <input type="text" name="price" >
+
+  <input type="button" name="submit" value="submit">
+</form>
